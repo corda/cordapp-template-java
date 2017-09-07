a13c2bedeacda785f7a4f406940cc7e2149f5e5e
joeldudleyr3 <joel.dudley@r3.com>
2017-09-07 10:59:12 +0100

    Addresses review comments.

--- src/test/java/com/template/flow/FlowTests.java
+++ src/test/java/com/template/flow/FlowTests.java
@@ -7,7 +7,6 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
 
-
 public class FlowTests {
     private MockNetwork net;
     private MockNetwork.MockNode a;
