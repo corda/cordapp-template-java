deeac11b7da087f8a4c46d6ae2570585e5250517
joeldudleyr3 <joel.dudley@r3.com>
2018-11-07 14:48:30 +0000

    Renames controller function.

--- clients/src/main/java/com/template/webserver/Controller.java
+++ clients/src/main/java/com/template/webserver/Controller.java
@@ -21,7 +21,7 @@ public class Controller {
     }
 
     @GetMapping(value = "/templateendpoint", produces = "text/plain")
-    private String status() {
+    private String templateendpoint() {
         return "Define an endpoint here.";
     }
 }
