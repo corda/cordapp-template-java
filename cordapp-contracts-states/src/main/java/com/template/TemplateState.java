9a2fcc3583cbae3562ef3ee70f41fc88976f449e
joeldudleyr3 <joel.dudley@r3.com>
2018-11-12 11:51:12 +0000

    Participants no longer passed in constructor.

--- cordapp-contracts-states/src/main/java/com/template/TemplateState.java
+++ cordapp-contracts-states/src/main/java/com/template/TemplateState.java
@@ -3,20 +3,20 @@ package com.template;
 import net.corda.core.contracts.ContractState;
 import net.corda.core.identity.AbstractParty;
 
+import java.util.Arrays;
 import java.util.List;
 
 // *********
 // * State *
 // *********
 public class TemplateState implements ContractState {
-    private List<AbstractParty> participants;
 
-    public TemplateState(List<AbstractParty> participants) {
-        this.participants = participants;
+    public TemplateState() {
+
     }
 
     @Override
     public List<AbstractParty> getParticipants() {
-        return participants;
+        return Arrays.asList();
     }
 }
