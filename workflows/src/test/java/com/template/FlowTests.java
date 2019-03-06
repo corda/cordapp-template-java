f51a75910ee3010e01a054a2e0a4b47311c0d2ab
ben <gitlab@nimmaj.com>
2019-03-06 15:13:14 +0000

    updated to corda 4, including the checked in quasar.jar

--- workflows/src/test/java/com/template/FlowTests.java
+++ workflows/src/test/java/com/template/FlowTests.java
@@ -3,13 +3,18 @@ package com.template;
 import com.google.common.collect.ImmutableList;
 import com.template.flows.Responder;
 import net.corda.testing.node.MockNetwork;
+import net.corda.testing.node.MockNetworkParameters;
 import net.corda.testing.node.StartedMockNode;
+import net.corda.testing.node.TestCordapp;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
 public class FlowTests {
-    private final MockNetwork network = new MockNetwork(ImmutableList.of("com.template"));
+    private final MockNetwork network = new MockNetwork(new MockNetworkParameters(ImmutableList.of(
+        TestCordapp.findCordapp("com.template.contracts"),
+        TestCordapp.findCordapp("com.template.flows")
+    )));
     private final StartedMockNode a = network.createNode();
     private final StartedMockNode b = network.createNode();
 
