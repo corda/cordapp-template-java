4d518c567076504128230d38c0cdf6beb7aa6d71
Mike Tery <mike.tery@r3.com>
2022-09-27 13:40:34 -0400

    added db port in build.gradle, working on flow

--- workflows/src/test/java/com/titles/FlowTests.java
+++ workflows/src/test/java/com/titles/FlowTests.java
@@ -18,6 +18,7 @@ public class FlowTests {
     private MockNetwork network;
     private StartedMockNode a;
     private StartedMockNode b;
+    private StartedMockNode county;
 
     @Before
     public void setup() {
@@ -25,8 +26,9 @@ public class FlowTests {
                 TestCordapp.findCordapp("com.titles.contracts"),
                 TestCordapp.findCordapp("com.titles.flows")))
                 .withNotarySpecs(ImmutableList.of(new MockNetworkNotarySpec(CordaX500Name.parse("O=Notary,L=London,C=GB")))));
-        a = network.createPartyNode(null);
-        b = network.createPartyNode(null);
+        a = network.createPartyNode(CordaX500Name.parse("O=Alice,L=NYC,C=US"));
+        b = network.createPartyNode(CordaX500Name.parse("O=Bob,L=NYC,C=US"));
+        county = network.createPartyNode(CordaX500Name.parse("O=County,L=NYC,C=US"));
         network.runNetwork();
     }
 
@@ -39,14 +41,17 @@ public class FlowTests {
     public void dummyTest() throws Exception {
         TitleIssueFlow.TitleFlowInitiator flow = new TitleIssueFlow.TitleFlowInitiator(
                 a.getInfo().getLegalIdentities().get(0),
-                b.getInfo().getLegalIdentities().get(0),
-                "1234567890", "parcelId");
-        Future<SignedTransaction> future = a.startFlow(flow);
+                county.getInfo().getLegalIdentities().get(0),
+                "1 West St New York NY USA 10004", "parcelId");
+        Future<SignedTransaction> future = county.startFlow(flow);
         network.runNetwork();
 
         //successful query means the state is stored at node b's vault. Flow went through.
-//        QueryCriteria.VaultQueryCriteria inputCriteria = new QueryCriteria.VaultQueryCriteria().withStatus(Vault.StateStatus.UNCONSUMED);
-//        TitleState state = a.getServices().getVaultService().queryBy(TitleState.class, inputCriteria)
-//                .getStates().get(0).getState().getData();
+        QueryCriteria.VaultQueryCriteria inputCriteria = new QueryCriteria.VaultQueryCriteria(
+//                .withStatus(
+                Vault.StateStatus.UNCONSUMED);
+        TitleState state = a.getServices().getVaultService().queryBy(TitleState.class)
+                .getStates().get(0).getState().getData();
+        System.out.print(99);
     }
 }
